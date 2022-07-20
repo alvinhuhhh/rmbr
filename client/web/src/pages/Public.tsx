527bb498404b5ba1fd3b66b327697118e9c2dda6
Alvin Tan <alvin.tan1912@gmail.com>
2022-07-20 23:41:57 +0800

    Redesign public landing page

--- client/web/src/pages/Public.tsx
+++ client/web/src/pages/Public.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react";
-import { Grid } from "@mui/material";
+import React, { useEffect, useContext } from "react";
+import { Grid, Paper, Typography, Link, IconButton } from "@mui/material";
+import { LightMode as LightModeIcon, DarkMode as DarkModeIcon } from "@mui/icons-material";
+import { ThemeContext } from "../contexts/ThemeContext";
 import { useNavigate } from "react-router-dom";
 import { decodeJwt } from "jose";
 import LoginService from "../services/Login";
@@ -9,6 +11,7 @@ declare global {
 }
 
 export default function Public({ ...props }: LoginProps) {
+  const theme = useContext(ThemeContext);
   const navigate = useNavigate();
 
   const handleCallback = (response: { credential: any }) => {
@@ -46,7 +49,42 @@ export default function Public({ ...props }: LoginProps) {
 
   return (
     <Grid container justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
-      <div id="signInDiv" />
+      <IconButton
+        size="small"
+        color="inherit"
+        onClick={theme.toggleDarkMode}
+        sx={{ position: "absolute", top: 15, right: 15 }}
+      >
+        {theme.mode === "dark" ? <LightModeIcon /> : <DarkModeIcon />}
+      </IconButton>
+      <Grid item container justifyContent="center" alignItems="center" spacing={5}>
+        <Grid item sm={12} lg={6}>
+          <Typography variant="h3" marginLeft={5}>
+            Jabberwocky
+          </Typography>
+          <Typography variant="h6" marginLeft={5}>
+            Because every developer needs to make a todo app
+          </Typography>
+        </Grid>
+        <Grid item sm={12} lg={6} container justifyContent="center" alignItems="center">
+          <div id="signInDiv" />
+        </Grid>
+      </Grid>
+      <Grid item sx={{ position: "absolute", bottom: 0, width: "100vw" }}>
+        <Paper sx={{ height: "100%", width: "100%" }}>
+          <Grid container justifyContent="space-between" padding={3}>
+            <Grid item>Jabberwocky</Grid>
+            <Grid item>
+              <Link href="https://github.com/alvinhuhhh/jabberwocky" color="inherit">
+                Github
+              </Link>
+              <Typography variant="body1" color="inherit" marginLeft={3} display="inline">
+                Copyright © Tan Wei Bing Alvin
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
     </Grid>
   );
 }
