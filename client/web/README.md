1a2b62e1c3c93aabe2b0f8f7ddcd865ed1f95965
Alvin Tan <alvin.tan1912@gmail.com>
2022-07-23 21:32:25 +0800

    Reconfigure deployment Dockerfile and build scripts

--- client/web/README.md
+++ client/web/README.md
@@ -1,5 +1,11 @@
 # Configuration
 
-## Required environment variables
+## Required configuration
 
+Configure a .env.production file before build, as React embeds environment variables into the static files on build that cannot be changed during runtime.
+
+### Variables to configure:
+
+- PORT
 - REACT_APP_API_URL
+- REACT_APP_CLIENT_ID
--- terraform/main.tf
+++ terraform/main.tf
@@ -29,15 +29,3 @@ module "vpc" {
   public_cidr  = "10.0.0.0/28"
   private_cidr = "10.0.0.16/28"
 }
-
-# Create EC2 instances
-module "ec2" {
-  source = "./modules/ec2"
-
-  jabberwocky_vpc_id            = module.vpc.jabberwocky_vpc_id
-  jabberwocky_public_subnet_id  = module.vpc.jabberwocky_public_subnet_id
-  jabberwocky_private_subnet_id = module.vpc.jabberwocky_private_subnet_id
-
-  ec2_ami           = "ami-0c802847a7dd848c0"
-  ec2_instance_type = "t2.micro"
-}
