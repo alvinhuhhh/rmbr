3f4a1350f78e5dbb119c652e1b43ff0dad41cad3
Alvin Tan <alvin.tan1912@gmail.com>
2022-08-09 16:29:24 +0800

    Added SSL to API

--- api/todo/app.ts
+++ api/todo/app.ts
@@ -1,49 +1,53 @@
 import "dotenv/config";
+import fs from "fs";
+import https from "https";
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 import verify from "./middleware/verify";
 
 import routes from "./routes/index";
 
 const app = express();
+const options = {
+  key: fs.readFileSync("server.key", "utf-8"),
+  cert: fs.readFileSync("server.crt", "utf-8"),
+};
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 app.use(morgan("common"));
+app.use(helmet());
 app.use(verify);
 
 // Routes
 app.use("/", routes);
 
 try {
-  // Connect to DB
+  // Connect to DB and start listening
   if (typeof process.env.DB_CONNECTION_STRING === "string") {
     const db = process.env.DB_CONNECTION_STRING;
 
     mongoose.connect(db);
     const database = mongoose.connection;
+
     database.on("error", (error) => {
       throw new Error(error);
     });
+
     database.once("connected", () => {
       console.log("Database connected");
+      app.listen(3000);
+      https.createServer(options, app).listen(443, () => {
+        console.log(`API server started and listening on https://localhost`);
+      });
     });
   } else {
     throw new Error("DB_CONNECTION_STRING is invalid.");
-  }
-
-  // Start listening
-  if (typeof process.env.PORT === "string") {
-    const port = parseInt(process.env.PORT);
-
-    app.listen(port, () => {
-      console.log(`Todo API started and listening on http://localhost:${port}`);
-    });
-  } else {
-    throw new Error("PORT is invalid.");
   }
 } catch (err) {
   console.log(err);
